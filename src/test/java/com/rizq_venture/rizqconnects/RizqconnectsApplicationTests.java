4d2d702a036e0b53ddec378047de891c58e24fec
Aman Shaikh <amanshaikh05050@gmail.com>
2025-10-30 09:36:56 +0530

    Added initial setup

---
+++ src/test/java/com/rizq_venture/rizqconnects/RizqconnectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizq_venture.rizqconnects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizqconnectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
